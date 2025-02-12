3158df07981c42a7926660ea39d079e1f94170ae
DESKTOP-TG3F8QK\Administrator <d1i2n3g4@qq.com>
2025-02-12 23:27:26 +0800

    增加资源，和增加部分的数据

---
+++ Client/rdiusrumbleclient/objects/actor/actor.gd
@@ -0,0 +1,63 @@
+extends Area2D
+
+const packets := preload("res://packets/packets.gd")
+
+const Scene := preload("res://objects/actor/actor.tscn")
+const Actor := preload("res://objects/actor/actor.gd")
+
+var actor_id: int
+var actor_name: String
+var start_x: float
+var start_y: float
+var start_rad: float
+var speed: float
+var is_player: bool
+
+var velocity: Vector2
+var radius: float
+
+@onready var _nameplate: Label = $Label
+@onready var _camera: Camera2D = $Camera2D
+@onready var _collision_shape: CircleShape2D = $CollisionShape2D.shape
+
+static func instantiate(actor_id: int, actor_name: String, x: float, y: float, radius: float, speed: float, is_player: bool) -> Actor:
+	var actor := Scene.instantiate()
+	actor.actor_id = actor_id
+	actor.actor_name = actor_name
+	actor.start_x = x
+	actor.start_y = y
+	actor.start_rad = radius
+	actor.speed = speed
+	actor.is_player = is_player
+
+	return actor
+
+
+func _ready():
+	position.x = start_x
+	position.y = start_y
+	velocity = Vector2.RIGHT * speed
+	radius = start_rad
+	
+	_collision_shape.radius = radius
+	_nameplate.text = actor_name
+
+func _physics_process(delta) -> void:
+	position += velocity * delta
+	
+	if not is_player:
+		return
+	# Player-specific stuff below here
+		
+	var mouse_pos := get_global_mouse_position()
+	
+	var input_vec = position.direction_to(mouse_pos).normalized()
+	if abs(velocity.angle_to(input_vec)) > TAU / 15: # 24 degrees
+		velocity = input_vec * speed
+		var packet := packets.Packet.new()
+		var player_direction_message := packet.new_player_direction()
+		player_direction_message.set_direction(velocity.angle())
+		WS.send(packet)
+
+func _draw() -> void:
+	draw_circle(Vector2.ZERO, _collision_shape.radius, Color.DARK_ORCHID)
--- Client/rdiusrumbleclient/states/ingame/ingame.gd
+++ Client/rdiusrumbleclient/states/ingame/ingame.gd
@@ -1,9 +1,13 @@
 extends Node
 
 const packets := preload("res://packets/packets.gd")
+const Actor := preload("res://objects/actor/actor.gd")
+
 
 @onready var _line_edit: LineEdit = $UI/LineEdit
 @onready var _log: Log = $UI/Log
+
+@onready var _world: Node2D = $World
 
 func _ready() -> void:
 	WS.connection_closed.connect(_on_ws_connection_closed)
@@ -19,6 +23,8 @@ func _on_ws_packet_received(packet: packets.Packet) -> void:
 	var sender_id := packet.get_sender_id() 
 	if packet.has_chat():
 		_handle_chat_msg(sender_id,packet.get_chat())
+	elif packet.has_player():
+		_handle_player_msg(sender_id, packet.get_player())
 		
 func _handle_chat_msg(sender_id:int,chat_msg:packets.ChatMessage) ->void:
 	_log.chat("Client %d" % sender_id,chat_msg.get_msg())
@@ -38,3 +44,17 @@ func _on_line_edit_text_submitted(new_text:String) -> void:
 		_log.chat("You",new_text)
 	
 	_line_edit.clear()
+	
+#收到玩家进入的消息
+func _handle_player_msg(sender_id: int, player_msg: packets.PlayerMessage) -> void:
+	var actor_id := player_msg.get_id()
+	var actor_name := player_msg.get_name()
+	var x := player_msg.get_x()
+	var y := player_msg.get_y()
+	var radius := player_msg.get_radius()
+	var speed := player_msg.get_speed()
+
+	var is_player := actor_id == GameManager.client_id
+	
+	var actor := Actor.instantiate(actor_id, actor_name, x, y, radius, speed, is_player)
+	_world.add_child(actor)
